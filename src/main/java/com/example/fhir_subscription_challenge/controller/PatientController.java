b6add7c50f151add53513f7397516894d7bce8b4
Markus Wieczorek <markus.wieczorek@healex.systems>
2022-01-21 10:02:23 +0100

    fix: Handle connection exception
    Now catches a client connection exception in case the fhir client is not available. previously this would throw and return a 500.

--- src/main/java/com/example/fhir_subscription_challenge/controller/PatientController.java
+++ src/main/java/com/example/fhir_subscription_challenge/controller/PatientController.java
@@ -1,12 +1,13 @@
 package com.example.fhir_subscription_challenge.controller;
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.api.MethodOutcome;
-import ca.uhn.fhir.rest.client.api.IGenericClient;
+import ca.uhn.fhir.rest.client.exceptions.FhirClientConnectionException;
 import com.example.fhir_subscription_challenge.services.PatientService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,15 +24,20 @@ public class PatientController {
             consumes = {"text/plain"},
             produces = {"text/plain"})
     @ResponseStatus(HttpStatus.CREATED)
-    public void createPatient(@RequestParam String familyName, @RequestParam String givenName) {
+    public ResponseEntity<String> createPatient(@RequestParam String familyName, @RequestParam String givenName) {
+        try {
+            FhirContext ctx = FhirContext.forR4();
+            var client = ctx.newRestfulGenericClient("http://localhost:8090/fhir/");
 
-        FhirContext ctx = FhirContext.forR4();
-        IGenericClient client = ctx.newRestfulGenericClient("http://localhost:8090/fhir/");
+            var patient = patientService.CreatePatient(familyName, givenName);
+            MethodOutcome result = client.create().resource(patient).prettyPrint().encodedJson().execute();
 
-        var patient = patientService.CreatePatient(familyName, givenName);
-        MethodOutcome result = client.create().resource(patient).prettyPrint().encodedJson().execute();
-
-        logger.info(ctx.newJsonParser().setPrettyPrint(true).encodeResourceToString(patient));
-        logger.info("Created Patient: %s".formatted(result.getId().getValue()));
+            logger.info(ctx.newJsonParser().setPrettyPrint(true).encodeResourceToString(patient));
+            logger.info("Created Patient: %s".formatted(result.getId().getValue()));
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (FhirClientConnectionException exception) {
+            logger.error(exception.getMessage());
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_GATEWAY);
+         }
     }
 }
